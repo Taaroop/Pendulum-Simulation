e7aeb1c833b5f0bc1cc949c648fd04b7f8446421
Taaroop <67786757+Taaroop@users.noreply.github.com>
2022-08-14 22:47:31 +0600

    Add files via upload

---
+++ pendulum_simulation.py
@@ -0,0 +1,27 @@
+import math
+import matplotlib.pyplot as plt
+
+length = 10
+g = 9.8
+theta = 30
+velo = 0
+sim_time = 100
+
+dt = 0.1
+t_elapsed = 0
+
+li_time = [0]
+li_angle = [theta]
+
+while t_elapsed < sim_time:
+    acc = g*math.sin((math.pi/180)*theta)
+    velo = velo + (acc*dt)
+    d_theta = (velo*dt)/length
+    theta = theta-d_theta
+    t_elapsed += dt
+    print(acc)
+    
+    li_time.append(t_elapsed)
+    li_angle.append(theta)
+
+plt.plot(li_time, li_angle)
